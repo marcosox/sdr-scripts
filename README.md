8ed8fa41e363d683ca5b02cc1fe79fef3c02f316
sox <2-sox@users.noreply.computer0.local>
2022-08-03 02:43:47 +0200

    first commit
    Signed-off-by: sox <2-sox@users.noreply.computer0.local>

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # sdr-scripts
+
 Scripts related to rtl_sdr tools
+
+- frequency_scan.py: Used to remotely control [rtl_udp](https://github.com/sysrun/rtl-sdr)
+  to scan a frequency range with a defined step and interval
+- record_and_convert.sh: Shell script which records and encodes to wav file,
+  while streaming audio via udp
---
+++ record_and_stream.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Receives, streams udp raw samples and exports to wav at the end
+# Change the values below as necessary
+
+FREQUENCY=88000000
+SQUELCH=0
+WAV_PATH=/path/to/folder
+DURATION=12.0h
+MODULATION=fm
+STREAMING_HOST=127.0.0.1
+STREAMING_PORT=7355
+
+TIMESTAMP=$(date +%Y%m%d-%H%M)
+RAW_FILE=${FREQUENCY}_${TIMESTAMP}.raw
+OUT_FILE=${WAV_PATH}/${TIMESTAMP}_${FREQUENCY}.wav
+
+echo "Recording for ${DURATION}..."
+timeout ${DURATION} rtl_fm -f ${FREQUENCY} -l ${SQUELCH} -M ${MODULATION} | tee "${RAW_FILE}" | timeout ${DURATION} nc -u ${STREAMING_HOST} ${STREAMING_PORT}
+# compand is used to limit loud noises from powerful sources by compressing the output audio signal
+sox -traw -r24k -es -b16 -c1 -V1 "${RAW_FILE}" -twav "${OUT_FILE}" compand 0.2,0.5 6:-70,-60,-5 -15 -90 0.2 && rm "${RAW_FILE}"
